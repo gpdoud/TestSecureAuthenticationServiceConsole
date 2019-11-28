737ef3fe4dbc2b67051937715589182385f5bd03
Greg Doud <gpdoud@gmail.com>
2019-11-28 09:26:46 -0500

    Add project files.

---
+++ TestSecureAuthenticationServiceConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TestSecureAuthenticationServiceConsole {
+
+    class Program {
+
+        static readonly HttpClient http = new HttpClient();
+        static readonly string baseUrl = "http://localhost:7015/api";
+    
+        static async Task Main(string[] args) {
+            // GetAll()
+            var users = await GetAll();
+            Console.WriteLine("users: [");
+            foreach(var u in users)
+                Console.WriteLine($"  {u}");
+            Console.WriteLine("]");
+            // GetPk()
+            //var user = await GetPk(3);
+            //Console.WriteLine($"user: {user}");
+            // Insert()
+            //var newUser = new User {
+            //    Id = 0, Username = "us", Password = "us", Name = "User", Active = true
+            //};
+            //var res = await Insert(newUser);
+            //Console.WriteLine($"Insert: {res}");
+        }
+        static async Task<IEnumerable<User>> GetAll() {
+            var res = await http.GetStringAsync($"{baseUrl}/users");
+            var options = new JsonSerializerOptions {
+                WriteIndented = true
+            };
+            var users = (IEnumerable<User>)JsonSerializer.Deserialize<IEnumerable<User>>(res, options);
+            return users;
+        }
+        static async Task<User> GetPk(int id) {
+            var res = await http.GetStringAsync($"{baseUrl}/users/{id}");
+            var options = new JsonSerializerOptions {
+                WriteIndented = true
+            };
+            var user = (User)JsonSerializer.Deserialize<User>(res, options);
+            return user;
+        }
+        static async Task<HttpResponseMessage> Insert(User usr) {
+            var userJson = JsonSerializer.Serialize<User>(usr);
+            var content = new StringContent(userJson, System.Text.Encoding.Default, "application/json");
+            var res = await http.PostAsync($"{baseUrl}/users", content);
+            return res;
+        }
+    }
+}
---
+++ TestSecureAuthenticationServiceConsole/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace TestSecureAuthenticationServiceConsole {
+    
+    public class User {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("securityKey")]
+        public string SecurityKey { get; set; } // Guid changable by user
+        [JsonPropertyName("active")]
+        public bool Active { get; set; } = true;
+        [JsonPropertyName("created")]
+        public DateTime Created { get; set; } = DateTime.Now;
+        [JsonPropertyName("modified")]
+        public DateTime? Modified { get; set; } = null;
+        [JsonPropertyName("deleted")]
+        public DateTime? Deleted { get; set; } = null;
+
+        public string GetSecurityKey(string username, string password) {
+            if(this.Username.ToLower().Equals(username) && this.Password.Equals(password)) {
+                return this.SecurityKey;
+            }
+            return "Unauthorized!";
+        }
+
+        public string RegenSecurityKey() {
+            this.SecurityKey = Guid.NewGuid().ToString();
+            return this.SecurityKey;
+        }
+
+        public User() {
+            this.RegenSecurityKey();
+        }
+
+        public override string ToString() {
+            return $"{{ id: {Id}, username: {Username}, password: {Password}, name: {Name}, active: {Active}, " +
+                $"securityKey: {SecurityKey}, created: {Created}, modified: {(Modified==null ? "null" : Modified.ToString())}, deleted: {(Deleted==null ? "null" : Deleted.ToString())} }}";
+        }
+    }
+}
